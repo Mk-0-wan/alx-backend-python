d89ba716656892470773152888a32153db4bc30a
gh057 <victormurithi2020@gmail.com>
2024-07-02 05:42:42 +0300

    added the new test case for get_json

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -2,7 +2,8 @@
 
 """Simple test case for nested map"""
 import unittest
-from utils import access_nested_map
+from unittest.mock import Mock, patch
+from utils import access_nested_map, get_json
 from parameterized import parameterized
 
 
@@ -25,3 +26,25 @@ class TestAccessNestedMap(unittest.TestCase):
         """Handling all the key errors passed"""
         with self.assertRaises(err):
             access_nested_map(nested_map, path)
+
+
+class TestGetJson(unittest.TestCase):
+    """Testing the get_json method"""
+
+    @parameterized.expand([
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False}),
+    ])
+    @patch('requests.get')
+    def test_get_json(self, test_url, test_payload, mock_get):
+        """Test get_json returns the expected result"""
+        mock_response = Mock()
+        mock_response.json.return_value = test_payload
+        mock_get.return_value = mock_response
+
+        # Call get_json with test_url and assert the result
+        result = get_json(test_url)
+        self.assertEqual(result, test_payload)
+
+        # Assert that requests.get was called once with the test_url
+        mock_get.assert_called_once_with(test_url)
