24a13ac4779b6cea17a913e77845247f636c79e4
gh057 <victormurithi2020@gmail.com>
2024-07-03 08:20:23 +0300

    another test_case

--- 0x03-Unittests_and_integration_tests/test_client.py
+++ 0x03-Unittests_and_integration_tests/test_client.py
@@ -47,3 +47,30 @@ class TestGithubOrgClient(unittest.TestCase):
         result = client._public_repos_url
 
         self.assertEqual(result, "https://api.github.com/orgs/google/repos")
+
+    @patch('client.get_json')
+    def test_public_repos(self, mock_get_json):
+        """Test GithubOrgClient.public_repos returns the correct value"""
+        test_payload = [
+            {"name": "repo1"},
+            {"name": "repo2"},
+            {"name": "repo3"}
+        ]
+        mock_get_json.return_value = test_payload
+        test_url = "https://api.github.com/orgs/google/repos"
+
+        with patch(
+                'client.GithubOrgClient._public_repos_url',
+                new_callable=PropertyMock) as mock_repos_url:
+            mock_repos_url.return_value = test_url
+
+            client = GithubOrgClient("google")
+            result = client.public_repos()
+
+            # Check that public_repos returns
+            # the correct list of repository names
+            self.assertEqual(result, ["repo1", "repo2", "repo3"])
+
+            # Ensure the mocked property and get_json were called once
+            mock_repos_url.assert_called_once()
+            mock_get_json.assert_called_once_with(test_url)
